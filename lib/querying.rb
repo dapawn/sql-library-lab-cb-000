6c8b6d6a936c9da83fa63ef5ebe4ce6ef033151c
David Kuruvilla <dapawn@gmail.com>
2018-01-04 16:50:01 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,22 +1,37 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books WHERE books.series_id = 1;"
+  "SELECT books.title, books.year 
+   FROM books 
+   WHERE books.series_id = 1;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1;"
+  "SELECT name, motto 
+   FROM characters 
+   ORDER BY LENGTH(motto) DESC 
+   LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1;"
+  "SELECT species, COUNT(species) 
+   FROM characters 
+   GROUP BY species 
+   ORDER BY COUNT(species) DESC 
+   LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM series JOIN authors ON authors.id = series.author_id JOIN subgenres ON subgenres.id = series.subgenre_id;"
+  "SELECT authors.name, subgenres.name 
+   FROM series JOIN authors ON authors.id = series.author_id JOIN subgenres ON subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN characters ON series.id = characters.series_id WHERE species = \"human\" GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1 ;"
+  "SELECT series.title 
+   FROM series JOIN characters ON series.id = characters.series_id 
+   WHERE species = \"human\" 
+   GROUP BY series.title 
+   ORDER BY COUNT(*) DESC 
+   LIMIT 1 ;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
